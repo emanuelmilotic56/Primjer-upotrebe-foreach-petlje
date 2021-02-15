34f9e4f772334d01b8845a9ef8ade2d8a20f7058
emanuelmilotic56 <58472080+emanuelmilotic56@users.noreply.github.com>
2021-02-15 08:53:17 +0100

    Add files via upload

---
+++ Primjer upotrebe foreach petlje i objekta-EM/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Primjer_upotrebe_foreach_petlje_i_objekta_EM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> brojevi = new List<int>();
+
+            while (true)
+            {
+                Console.Write("Unesite cijeli broj:");
+                int broj = int.Parse(Console.ReadLine());
+
+                if (broj == 0)
+                {
+                    break;
+                }
+
+                brojevi.Add(broj);
+            }
+            brojevi.Sort();
+            brojevi.Reverse();
+
+
+            Console.WriteLine("Uneseni brojevi pad.redoslijedom:");
+            foreach (int broj in brojevi)
+            {
+                Console.Write(broj + ", ");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
